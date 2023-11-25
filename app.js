3d70c74c77d67a1de796935db5801a58259496f1
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-25 11:53:03 +0700

    Setup counter function

--- app.js
+++ app.js
@@ -8,8 +8,28 @@ function Counter(element, value) {
   this.valueDOM.textContent = this.value;
 }
 
+Counter.prototype.increase = function () {
+  console.log(this);
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
 const firstCounter = new Counter(getElement(".first-counter"), 100);
 const secondCounter = new Counter(getElement(".second-counter"), 200);
+
+firstCounter.increase();
+firstCounter.increase();
+firstCounter.increase();
+firstCounter.decrease();
+secondCounter.reset();
 
 function getElement(selection) {
   const element = document.querySelector(selection);
