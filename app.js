fee284c4f7b7a128526c0b16ab9e7a94c10315e1
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-25 11:49:11 +0700

    Select elements

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 function Counter(element, value) {
-  console.log(element, value);
+  this.counter = element;
+  this.value = value;
+  this.resetBtn = element.querySelector(".reset");
+  this.increaseBtn = element.querySelector(".increase");
+  this.decreaseBtn = element.querySelector(".decrease");
+  this.valueDOM = element.querySelector(".value");
+  this.valueDOM.textContent = this.value;
 }
 
 const firstCounter = new Counter(getElement(".first-counter"), 100);
